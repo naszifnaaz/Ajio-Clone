811e982503981e9d19185090027112f12be59a92
Mayurwagh98 <mswagh1998@gmail.com>
2022-03-31 02:47:32 +0530

    login-authentication

--- backend/login_authentication/src/configs/db.js
+++ backend/login_authentication/src/configs/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 
 module.exports = () => {
-    return mongoose.connect("mongodb+srv://MayurWagh:mayur_123@cluster0.47rnh.mongodb.net/projetc_authenctication?retryWrites=true&w=majority");
+    return mongoose.connect("");
 };
